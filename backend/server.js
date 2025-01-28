a1ebbdd9c2b2446a67c8ec6ccc57cd0f09758852
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2025-01-28 16:44:49 -0500

    feat: :zap: enlazamos backend con frontend
    instalamos morgan y ejs para enlazar backend con frontend y creando un ejs como html

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// Directorio actual
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('views', join(__dirname, '..', 'frontend', 'views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res) => {
-    res.send("Hello World")
+    res.render('index')
 })
 
 app.listen(5000, () => {
