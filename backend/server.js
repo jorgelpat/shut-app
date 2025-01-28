8dad3fba3a1b28edebf6a3aea0642c5687237989
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2025-01-28 17:09:23 -0500

    feat: :zap: pasamos la pagina para importarla desde otro modulo

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
+import indexRoutes from './routes/auth.routes.js'
+
 const app = express();
 
 // Directorio actual
@@ -9,10 +11,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join(__dirname, '..', 'frontend', 'views'))
 app.set('view engine', 'ejs')
-
-app.get('/', (req,res) => {
-    res.render('index', {title: 'Shut-app'})
-})
+// importa la ruta de auth.routes.js
+app.use(indexRoutes)
 
 app.listen(5000, () => {
     console.log(`Server running on port 5000`)
